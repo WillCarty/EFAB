3857f38cc0a6842033c840792e4b3e17ae653a49
RobertWhite7 <robertwhite@robertwhitembp.local>
2016-11-01 09:51:22 -0400

    Added comments

--- EFAB/Models/Test.swift
+++ EFAB/Models/Test.swift
@@ -10,15 +10,18 @@ import Foundation
 import Alamofire
 import Freddy
 
+// Just a test object to excercise the network stack
 class Test : NetworkModel {
- 
+// properties we got from http://jsonplacehoder.typicode.com/posts
     var userId : Int?
     var id : Int?
     var title : String?
     var body : String?
     
+    // empty constructor
     required init() {}
     
+    // create an object from JSON
     required init(json: JSON) throws {
         userId = try? json.getInt(at: Constants.Test.userId)
         id = try? json.getInt(at: Constants.Test.id)
@@ -26,18 +29,22 @@ class Test : NetworkModel {
         body = try? json.getString(at: Constants.Test.body)
     }
     
+    // Always return HTTP.GET
     func method() -> Alamofire.HTTPMethod {
         return .get
     }
     
+    // A sample path to a single post
     func path() -> String {
         return "/posts/4"
     }
 
+    // Demo object isn't being posted to a server, so just return nil
     func toDictionary() -> [String: AnyObject]? {
         return nil
     }
     
+    // Helper method for debugging
     func description() -> String {
         var text = ""
         text += "title: \(title ?? "")\n"
--- EFAB/Networking/NetworkModel.swift
+++ EFAB/Networking/NetworkModel.swift
@@ -10,11 +10,17 @@ import Foundation
 import Alamofire
 import Freddy
 
+// Describes what you need to make a network request and read it
 protocol NetworkModel: JSONDecodable {
+    // creat the object by reading JSON
     init(json: JSON) throws
+    // create an empty object
     init()
     
+    // what is the HTTP Method (Get/Post/Put/etc)
     func method() -> Alamofire.HTTPMethod
+    // REST URL to the resource i.i. http://server.com/post/1
     func path() -> String
+    // Convert the object to a dictionary for later conversion to JSON
     func toDictionary() -> [String: AnyObject]?
 }
