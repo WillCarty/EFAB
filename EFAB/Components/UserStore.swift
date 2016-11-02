f546eb9f30c2fb7590ed1acc795059ff7a8e5a00
RobertWhite7 <robertwhite@robertwhitembp.local>
2016-11-02 11:15:08 -0400

    User Store added

---
+++ EFAB/Components/UserStore.swift
@@ -0,0 +1,50 @@
+//
+//  UserStore.swift
+//  EFAB
+//
+//  Created by Robert White on 11/2/16.
+//  Copyright © 2016 Teky. All rights reserved.
+//
+
+import Foundation
+
+class UserStore {
+    // singleton
+    static let shared = UserStore()
+    private init() {}
+    
+    func login(_ loginUser: User, completion:@escaping (_ success:Bool, _ error: String?) -> Void){
+        
+        // Call web service to login
+        WebServices.shared.authUser(loginUser) { (user, error) in
+            if let user = user {
+                WebServices.shared.setAuthToken(user.token, expiration: user.expiration)
+                completion(true, nil)
+            }else {
+                completion(false, error)
+            }
+            
+        }
+        
+    }
+    
+    func register(_ registerUser: User, completion:@escaping(_ success:Bool, _ error: String?) -> Void){
+        
+        // Call web service to register
+        WebServices.shared.authUser(registerUser) { (user, error) in
+            if let user = user {
+                WebServices.shared.setAuthToken(user.token, expiration: user.expiration)
+                completion(true, nil)
+            }else {
+                completion(false, error)
+            }
+            
+        }
+        
+    }
+    
+    func logout( _ completion:() -> Void) {
+        WebServices.shared.clearUserAuthToken()
+        completion()
+    }
+}
