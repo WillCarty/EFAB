8978577d709bd0b7fd43018f64110d5cc284213b
RobertWhite7 <robertwhite@robertwhitembp.local>
2016-11-02 09:17:31 -0400

    added api

---
+++ EFAB/ViewControllers/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  EFAB
+//
+//  Created by Robert White on 11/1/16.
+//  Copyright © 2016 Teky. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+   
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
