2ec7cca55808e6c6be1ba9e5930480a8ffda984c
William Carty <will.carty@interapthq.com>
2016-11-03 11:50:11 -0400

    things are up and running with add category

--- EFAB/ViewControllers/AddCategoryViewController.swift
+++ EFAB/ViewControllers/AddCategoryViewController.swift
@@ -13,6 +13,7 @@ class AddCategoryViewController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var amountField: UITextField!
     
+    var currentTextField: UITextField?
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,14 +21,40 @@ class AddCategoryViewController: UIViewController {
     }
 
     @IBAction func saveTapped(_ sender: Any) {
+        saveCategory()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func saveCategory() {
+        currentTextField?.resignFirstResponder()
+        currentTextField = nil
+        
+        
+        guard let name = nameField.text , name != "" else {
+            present(Utils.createAlert(message: "You must provide a category name"), animated: true, completion: nil)
+            return
+        }
+        
+        guard let amount = amountField.text , amount != "" else {
+            present(Utils.createAlert(message: "You must provide an amount"), animated: true, completion: nil)
+            return
+        }
+        
+        guard let amountNumber = Double(amount) else {
+            present(Utils.createAlert(message: "You must provide a valid amount"), animated: true, completion: nil)
+            return
+        }
+        
+        let category = Category(name: name, amount: amountNumber)
+        WebServices.shared.postObject(category) { (object, error) in
+            if let _ = object {
+                self.present(Utils.createAlert("Success", message: "Category Created", dismissButtonTitle: "OK"), animated: true, completion: nil)
+            } else {
+                self.present(Utils.createAlert(message: "There was an error creating your category"), animated: true, completion: nil)
+            }
+        }
     }
     
-
+    
     /*
     // MARK: - Navigation
 
@@ -38,4 +65,26 @@ class AddCategoryViewController: UIViewController {
     }
     */
 
+}
+
+extension AddCategoryViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        currentTextField = textField
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == nameField {
+            amountField.becomeFirstResponder()
+        } else if textField == amountField {
+            saveCategory()
+        }
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == amountField {
+            return Utils.isNumber(string)
+        }
+        return true
+    }
 }
